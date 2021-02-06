8eb78842fb1c900330688575fba1efad661e318b
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-02-06 15:38:14 +0100

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3001
 
 app.get('/', (req, res) => {
-    const date = new Date().toLocaleString()
-    res.send(date)
+    res.sendFile(path.join(__dirname + '/test.html'));
 })
 
 app.listen(port, () => console.log('app listening on port 3001'))
