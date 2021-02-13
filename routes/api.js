12523b777a53e2cf4528e441fd979dd24a20e228
fafayouz <abdoufafa1921@gmail.com>
2021-02-13 01:52:58 -0900

    test update

--- routes/api.js
+++ routes/api.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.get('/users', ListUser)
 router.post('/adduser', AddUser)
 router.post('/deleteuser', DeleteUser)
+router.post('/updateuser',UpdateUser)
 
 module.exports = router;
